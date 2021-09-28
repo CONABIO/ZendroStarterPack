446fb85264a27469d4a15bc94fc8dd6ae9144272
jdonlucas <jdonlucas93@gmail.com>
2021-09-28 15:49:08 -0500

    add associated cumulus to jwt

--- graphql-server/utils/login.js
+++ graphql-server/utils/login.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt')
 const jsonwebtoken = require('jsonwebtoken')
 const path = require('path')
 const users = require(path.join(__dirname, '..', 'models', 'index.js')).user
+const cumulus = require(path.join(__dirname, '..', 'models', 'index.js')).cumulus
 const globals = require('../config/globals');
 
 module.exports = {
@@ -26,15 +27,30 @@ module.exports = {
     if (!valid) {
         throw new Error('Incorrect password')
     }
+    // roles
     const roles = await user_data.getRoles();
     console.log(roles)
     const name_roles = roles.map( x =>{ return x.name })
     console.log("ROLES: ", name_roles);
+    // cumulus
+    const cumulusIds = user_data.cumulus_ids.replace(/\[|\]/g, '').split(',').map(value => parseInt(value.replace(/"/g, '')));
+    let retrievedArray = [];
+    if(cumulusIds) {
+        retrievedArray = await Promise.all(
+          cumulusIds.map(async (c) => {
+            const retrievedCumulus = await cumulus.findOne({ where: { id: c }});
+            return { id: retrievedCumulus.id, name: retrievedCumulus.name};
+          })
+        )
+    }
+    console.log("CUMULUS: ", retrievedArray);
+
     // return json web token
     return jsonwebtoken.sign({
         id: user_data.id,
         username: user_data.username,
-        roles: name_roles
+        roles: name_roles,
+        cumulus: retrievedArray
     }, globals.JWT_SECRET, { expiresIn: '1h' })
   }
 
