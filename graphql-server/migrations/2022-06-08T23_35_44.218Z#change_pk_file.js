a974d824f9f12f086952e6e589a8233019bfcc6b
jdonlucas <jdonlucas93@gmail.com>
2022-06-08 18:39:20 -0500

    change file table

---
+++ graphql-server/migrations/2022-06-08T23_35_44.218Z#change_pk_file.js
@@ -0,0 +1,84 @@
+'use strict';
+const dict = require('../utils/graphql-sequelize-types');
+const {
+    Sequelize
+} = require("sequelize");
+const {
+    DOWN_MIGRATION
+} = require('../config/globals');
+/**
+ * @module - Migrations to create or to drop a table correpondant to a sequelize model.
+ */
+module.exports = {
+
+    /**
+     * up - Creates a table with the fields specified in the the createTable function.
+     *
+     * @param  {object} zendro initialized zendro object
+     */
+    up: async (zendro) => {
+        try {
+            const storageHandler = await zendro.models.file.storageHandler;
+            await storageHandler.getQueryInterface()
+                .dropTable('files')
+            await storageHandler.getQueryInterface()
+                .createTable('files', {
+                    id: {
+                        type: Sequelize[dict['Int']],
+                        primaryKey: true,
+                        autoIncrement: true
+                    },
+
+                    createdAt: {
+                        type: Sequelize.DATE
+                    },
+
+                    updatedAt: {
+                        type: Sequelize.DATE
+                    },
+
+                    url: {
+                        type: Sequelize[dict['String']]
+                    },
+                    id_alfresco: {
+                        type: Sequelize[dict['String']]
+                    },
+                    storage: {
+                        type: Sequelize[dict['String']]
+                    },
+                    updatedAt: {
+                        type: Sequelize[dict['DateTime']]
+                    },
+                    createdAt: {
+                        type: Sequelize[dict['DateTime']]
+                    },
+                    deployment_id: {
+                        type: Sequelize[dict['Int']]
+                    }
+
+                });
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+
+    /**
+     * down - Drop a table.
+     *
+     * @param  {object} zendro initialized zendro object
+     */
+    down: async (zendro) => {
+        try {
+            const storageHandler = await zendro.models.file.storageHandler;
+            const recordsExists = await zendro.models.file.count();
+            if (recordsExists && !DOWN_MIGRATION) {
+                throw new Error(`You are trying to delete all records of file and its associations. 
+            If you are sure about this, set environment variable 'DOWN_MIGRATION' to 'true' 
+            and re-execute this down-migration.`);
+            }
+            await storageHandler.getQueryInterface().dropTable('files');
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+};
