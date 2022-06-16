178bb3a66993f262f12a7a6aae479b35f8cf81ef
jdonlucas <jdonlucas93@gmail.com>
2022-06-16 18:08:39 -0500

    add unique constraint to alfresco_id

--- graphql-server/patches/file.js
+++ graphql-server/patches/file.js
@@ -1,11 +1,34 @@
-// Delete this file, if you do not want or need any patches for the given model.
+const validatorUtil = require('../utils/validatorUtil');
 
 // Dear user, edit the schema to adjust it to your model
 module.exports.logic_patch = function(file) {
 
-    // Write your patch code here
-    // Hint 1: file.prototype.function_name = function(...) {};
-    // Hint 2: file.prototype.property_name = {};
+    file.addOne = async (input) => {
+        //validate input
+        await validatorUtil.validateData('validateForCreate', file, input);
+        input = file.preWriteCast(input)
+        try {
+            // validate if unique alfresco_id
+            await file.findOne({ where: { id_alfresco: input.id_alfresco } })
+            .then(function (u) { 
+                if (u) {
+                    throw new Error('Alfresco id already registered!');  
+                }
+            });
+
+            const result = await file.sequelize.transaction(async (t) => {
+                let item = await file.create(input, {
+                    transaction: t
+                });
+                return item;
+            });
+            file.postReadCast(result.dataValues)
+            file.postReadCast(result._previousDataValues)
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
 
     return file;
 };
