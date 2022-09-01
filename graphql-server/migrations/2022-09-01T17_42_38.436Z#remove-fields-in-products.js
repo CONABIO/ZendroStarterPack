b3b3a392732ea3071ca2a625442543b59f4417cc
jdonlucas <jdonlucas93@gmail.com>
2022-09-01 14:30:59 -0500

    added migration to remove fields in product

---
+++ graphql-server/migrations/2022-09-01T17_42_38.436Z#remove-fields-in-products.js
@@ -0,0 +1,51 @@
+'use strict';
+const dict = require('../utils/graphql-sequelize-types');
+const {
+    Sequelize
+} = require("sequelize");
+const {
+    DOWN_MIGRATION
+} = require('../config/globals');
+/**
+ * @module - Migrations to create or to drop a table correpondant to a sequelize model.
+ */
+module.exports = {
+
+    /**
+     * up - Creates a table with the fields specified in the the createTable function.
+     *
+     * @param  {object} zendro initialized zendro object
+     */
+    up: async (zendro) => {
+        try {
+            const storageHandler = await zendro.models.product.storageHandler;
+            await storageHandler.getQueryInterface()
+                .removeColumn('products', 'audio_grid_id');
+            await storageHandler.getQueryInterface()
+                .removeColumn('products', 'audio_distance_to_mean');
+
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+
+    /**
+     * down - Drop a table.
+     *
+     * @param  {object} zendro initialized zendro object
+     */
+    down: async (zendro) => {
+        try {
+            const storageHandler = await zendro.models.product.storageHandler;
+            const recordsExists = await zendro.models.product.count();
+            if (recordsExists && !DOWN_MIGRATION) {
+                throw new Error(`You are trying to delete all records of product and its associations. 
+            If you are sure about this, set environment variable 'DOWN_MIGRATION' to 'true' 
+            and re-execute this down-migration.`);
+            }
+            await storageHandler.getQueryInterface().dropTable('products');
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+};
