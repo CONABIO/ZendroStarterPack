8cb6c4f18f96b001300a633f0b3265aa7fcaead5
jdonlucas <jdonlucas93@gmail.com>
2022-09-28 17:32:35 -0500

    added monkey patch for user model

--- graphql-server/patches/user.js
+++ graphql-server/patches/user.js
@@ -1,11 +1,58 @@
-// Delete this file, if you do not want or need any patches for the given model.
+const bcrypt = require('bcrypt');
+const validatorUtil = require('../utils/validatorUtil');
+const globals = require('../config/globals');
 
 // Dear user, edit the schema to adjust it to your model
 module.exports.logic_patch = function(user) {
 
-    // Write your patch code here
-    // Hint 1: user.prototype.function_name = function(...) {};
-    // Hint 2: user.prototype.property_name = {};
+    user.addOne = async (input) => {
+        //validate input
+        await validatorUtil.validateData('validateForCreate', user, input);
+        input = user.preWriteCast(input)
+        try {
+            let hash = await bcrypt.hash(input.password, globals.SALT_ROUNDS);
+            input.password = hash;
+            const result = await user.sequelize.transaction(async (t) => {
+                let item = await user.create(input, {
+                    transaction: t
+                });
+                return item;
+            });
+            user.postReadCast(result.dataValues)
+            user.postReadCast(result._previousDataValues)
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    user.updateOne = async (input) => {
+        //validate input
+        await validatorUtil.validateData('validateForUpdate', user, input);
+        input = user.preWriteCast(input)
+        try {
+            //check if password wants to be updated:
+            if(input.password !== undefined){
+                let hash = await bcrypt.hash(input.password, globals.SALT_ROUNDS);
+                input.password = hash;
+            }
+            let result = await user.sequelize.transaction(async (t) => {
+                let to_update = await user.findByPk(input[user.idAttribute()]);
+                if (to_update === null) {
+                    throw new Error(`Record with ID = ${input[user.idAttribute()]} does not exist`);
+                }
+                let updated = await to_update.update(input, {
+                    transaction: t
+                });
+                return updated;
+            });
+            user.postReadCast(result.dataValues)
+            user.postReadCast(result._previousDataValues)
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
 
     return user;
 };
